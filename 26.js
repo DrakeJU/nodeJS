04566747127e512c79fa3d42783a1d5589e74fe4
Drake Ju <32610409+DrakeJU@users.noreply.github.com>
2018-01-23 11:42:55 +0900

    수정
    라우터로 쓰겠다

--- 26.js
+++ 26.js
@@ -13,10 +13,12 @@ app.use(bodyParser.urlencoded({extended:false}));
 //false:String,array,  true:모든타입가능
 
 var router = express.Router();//경로관련
+
+app.use('/',router);
 router.route('/login').post(function (req,res) {
     console.log("/login경로 진입");
     var username = req.body.username;
     res.writeHead(200,{"Content-Type":"text/html;charset=utf8"});
     res.write("<h1>전달받은 username : "+username+"</h1>");
-    res.end;
+    res.end();
 });
